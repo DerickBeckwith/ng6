d6e4c15497875297222c322b2d4e28cbe351a7e8
Derick Beckwith <derick.beckwith@gmail.com>
2018-07-09 13:53:14 -0400

    Add new method getUsers() to the data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,4 +7,8 @@ import { HttpClient } from '@angular/common/http';
 export class DataService {
 
   constructor(private http: HttpClient) { }
+
+  getUsers() {
+    return this.http.get('https://jsonplaceholder.typicode.com/users');
+  }
 }
